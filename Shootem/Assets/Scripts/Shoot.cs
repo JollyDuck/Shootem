d933258395a79d03de56e9447ec787349c9c742c
JollyDuck <jolly_duck@hotmail.co.uk>
2014-09-07 20:25:37 +0100

    Started shooting, bullets dont move

---
+++ Shootem/Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			float mouseX = Input.GetAxis("Mouse X");
+			float mouseY = Input.GetAxis("Mouse Y");
+			Debug.Log ("MouseX: " + mouseX + " & MouseY: " + mouseY);
+		}
+		if(Input.GetKeyDown(KeyCode.Space)){
+			GameObject g = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
+
+		}
+		}
+
+
+}
