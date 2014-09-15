5356d90c482c9b5802c2907f3078865760805504
JollyDuck <jolly_duck@hotmail.co.uk>
2014-09-15 23:11:10 +0100

    Changed to Big Boat sim

---
+++ Shootem/Assets/Scripts/HelmMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelmMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeSpeed(float f){
+		
+	}
+}
---
+++ Shootem/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+	Rigidbody shipRigidBody;
+	public float forwardSpeed = 1;
+	public float rotateSpeed = 1;
+	// Use this for initialization
+	void Start () {
+		shipRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		if(Input.GetAxis("Vertical") != 0){
+			
+			shipRigidBody.AddRelativeForce(new Vector3(0,0,forwardSpeed*Input.GetAxis("Vertical")));
+			
+		}
+		if(Input.GetAxis ("Horizontal") != 0){
+			shipRigidBody.AddTorque(Vector3.up * Input.GetAxis ("Horizontal")* rotateSpeed);
+			//transform.Rotate(Vector3.up * Time.deltaTime *Input.GetAxis ("Horizontal") * rotateSpeed );
+		}
+		
+		if(shipRigidBody.velocity.magnitude>10) Debug.Log ("Ship's going real fast!");
+	}
+
+}
