5de7d68c87b5a0f90b4b51bb72a4bb1fb523d390
JollyDuck <jolly_duck@hotmail.co.uk>
2014-09-16 14:17:47 +0100

    Working helm control

--- Shootem/Assets/Scripts/HelmMove.cs
+++ Shootem/Assets/Scripts/HelmMove.cs
@@ -2,10 +2,18 @@
 using System.Collections;
 
 public class HelmMove : MonoBehaviour {
+	public float[] speeds = new float[7]; //-20, -10, 0, 25, 50, 75, 100
+	public float currentSpeed = 0f;
+	public float targetSpeed = 0f;
+	public float acceleration = 5f;
+	public float rotationSpeed = 0.05f;
+	public int rudderPosition = 0;
 
+	private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
@@ -14,6 +22,35 @@ public class HelmMove : MonoBehaviour {
 	}
 
 	public void changeSpeed(float f){
-		
+		targetSpeed = speeds[(int)f];
 	}
+
+	public void changeRotation(float f){
+		rudderPosition = (int)f;
+	}
+	// Physics update
+	void FixedUpdate() {
+		updateSpeed();
+		updateRotation();
+
+	}
+
+	private void updateSpeed(){
+		if(currentSpeed< targetSpeed){
+			rb.AddRelativeForce(new Vector3(0,0,acceleration));
+		}
+		else if(currentSpeed>targetSpeed){
+			rb.AddRelativeForce(new Vector3(0,0,-acceleration));
+		}
+
+		currentSpeed = rb.velocity.z;
+	}
+
+	private void updateRotation(){
+		rb.AddTorque(Vector3.up*rudderPosition*rotationSpeed);
+
+
+
+	}
+
 }
