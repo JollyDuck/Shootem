de341edf0967c779ba939e39a9e0756e5adf80a7
JollyDuck <jolly_duck@hotmail.co.uk>
2014-09-18 18:56:31 +0100

    Getting ready for MP

---
+++ Shootem/Assets/Scripts/RandomMatchMaker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMatchMaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("1.0");
+	
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby(){
+		Debug.Log ("Joined Lobby. Trying to Join Random Room...");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room! Creating one...");
+		PhotonNetwork.CreateRoom(null);
+
+	}
+
+	void OnCreatedRoom(){
+		Debug.Log ("Room created");
+	}
+}
